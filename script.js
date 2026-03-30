e488deef69817e34d84b29619d5dd69c71b6b834
VeronicaOrtCastillo <veronicaortcaz@gmail.com>
2026-03-30 14:56:07 -0600

    Codigo: Verificamos si los 3 números son iguales

--- script.js
+++ script.js
@@ -2,3 +2,11 @@
 let num1 = Number(prompt("Ingresa el primer número: "));
 let num2 = Number(prompt("Ingresa el segundo número: "));
 let num3 = Number(prompt("Ingresa el tercer número: "));
+
+//Debe ser capaz de identificar si los números son iguales e imprimir un mensaje por consola o por el DOM diciendo que los números son iguales.
+//Se realiza primero porque si los numero son iguales no se podra mostrar un numero mayor y uno menor
+//Utilizare === y && para comparar los valores de forma exacta
+if (num1 === num2 && num2 === num3){
+    //Si se cumple mostrara el siguente mensaje
+    console.log("Los números "+ num1 + num2 + num3 +" son iguales");
+} 
