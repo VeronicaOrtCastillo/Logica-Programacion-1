44eb7fe52df720d2d0a25285cf0dc4c6fda97ccd
VeronicaOrtCastillo <veronicaortcaz@gmail.com>
2026-04-09 17:48:34 -0600

    Codificacion script.js

--- Logica-Programacion-2/script.js
+++ Logica-Programacion-2/script.js
@@ -1,4 +1,15 @@
 //Funcion para realizar la conversion
 function convertirTemperatura(celsius){
-    //validacion para saber siel valo
+    //validacion para saber si el valor NO es numero
+    if (isNaN(celsius)){
+        return "Ingresa un numero válido";
+    }
+
+    //conversion a Fahrenheit
+    let fahrenheit = (celsius * 9/5) + 32;
+    //conversion a Kelvin
+    let kelvin = celsius + 273.15;
+
+    //retornamos los resultados
+    return "Kelvin: " + kelvin.toFixed(2) + " | Fahrenheit: " + fahrenheit.toFixed(2);
 }
